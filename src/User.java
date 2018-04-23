885d261a19feb0547904e15f45a77dd7b93d869b
hedixia <hedixia@umail.ucsb.edu>
2018-04-22 18:12:58 -0700

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,5 +1,55 @@
-/**
- * Created by brian on 4/22/2018.
- */
 public class User {
+	//Instance Var
+	String username;
+	String password;
+	int user_id;
+
+	//create profile
+	public create_profile ()
+	
+	//view profile 
+	public view_profile ()
+	
+	//modify profile 
+	public modify_profile ()
+	
+	//sign up a new account
+	public sign_up ()
+
+	//sign in an existing account
+	public sign_in()
+
+	//sign out current account 
+	public sign_out ()
+
+	//create a new schedule
+	public new_schedule ()
+
+	//get the infomation of a course
+	public find_course_info ()
+
+	//print out current schedule 
+	public print_current () 
+
+	//save current schedule 
+	public save_current ()
+
+	//get the list of classes to choose
+	public get_class_list ()
+	
+	//add a course to grid
+	public add_course ()
+
+	//delete a course from grid
+	public del_course ()
+	
+	//check current grid viability
+	public check_status ()
+	
+	//auto-complete grid 
+	public auto_complete ()
+	
+	//dismiss warning
+	public dismiss ()
+
 }
