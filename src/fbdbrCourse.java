1c5921269256535b416670abb1c47678e45ebbf1
hedixia <hedixia@umail.ucsb.edu>
2018-05-24 19:10:00 -0700

    Update fbdbrCourse.java

--- src/fbdbrCourse.java
+++ src/fbdbrCourse.java
@@ -2,7 +2,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
-public class fbdbrUser extends fbdbr{
+public class fbdbrCourse extends fbdbr{
     private Course tempCourse;
 
     public void retrieve () {
