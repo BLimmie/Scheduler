8df84945a54df320184a86bc90d07eb03d507658
HAL-9000 <bettercalljustin@gmail.com>
2018-06-03 18:49:53 -0700

    Some Ajax

--- src/main/webapp/Student.js
+++ src/main/webapp/Student.js
@@ -40,7 +40,7 @@ function initialize(){
         }
     });
 
-    document.getElementById("header").innerText = "Welcome, " + firstName + "!";
+    document.getElementById("header").innerText = "Welcome " + firstName + "!";
 
 
 }
