61ab59f7d05a38e37b67f9a676713e3a7755687e
hedixia <hedixia@umail.ucsb.edu>
2018-06-05 12:28:47 -0700

    Cheating with python

---
+++ src/main/java/servlet/fbdb.java
@@ -0,0 +1,55 @@
+package servlet;
+
+public class fbdb {
+    private String python;
+
+    public fbdb() {
+        this.python = "python";
+    }
+
+    public fbdb(String python) {
+        this.python = python;
+    }
+
+    public void pull(String filename, String nodestring) {
+        Runtime rt = Runtime.getRuntime();
+        String tempstr = "python jsonupdate ";
+        tempstr = tempstr + filename + " ";
+        tempstr = tempstr + "pull ";
+        tempstr = tempstr + nodestring;
+        try {
+            Process pr = rt.exec(tempstr);
+        } catch (Exception e) {
+        }
+    }
+
+    public void push(String filename, String nodestring) {
+        Runtime rt = Runtime.getRuntime();
+        String tempstr = "python jsonupdate ";
+        tempstr = tempstr + filename + " ";
+        tempstr = tempstr + "push ";
+        tempstr = tempstr + nodestring;
+        try {
+            Process pr = rt.exec(tempstr);
+        } catch (Exception e) {
+        }
+    }
+
+}
+
+/*
+import java.util.concurrent.TimeUnit
+
+String localpath = "./temp_12345"
+String fbdbpath = "Course cs48"
+
+#read
+fbdb().pull(localpath, fbdbpath)
+//wait
+TimeUnit.SECONDS.sleep(1);
+Course cs48 = JsonIo (localpath, Course.class).read()
+
+#write
+JsonIo (localpath, Course.class).write(cs48);
+fbdb().push(localpath, fbdbpath);
+*/
