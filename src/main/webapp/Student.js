7d0da8d340f97e96846df7ae35c03600f61bd4fd
HAL-9000 <bettercalljustin@gmail.com>
2018-06-05 21:16:37 -0700

    hi

--- src/main/webapp/Student.js
+++ src/main/webapp/Student.js
@@ -192,7 +192,7 @@ function Verify(){
 function AddCourses(quarterName, year, quarter){
     let courses = quarterName.childNodes;
     for (let i = 0; i < courses.length; i++){
-        if (courses[i].getAttribute("name") == "course") {
+        if ((courses[i].getAttribute("name")).localeCompare("course")) {
             let cname = courses[i].textContent;
             $.ajax({
                 type: "POST",
