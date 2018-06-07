957c8e33d80faab5fc5a633735ff28b4d29ffffc
Brian Lim <brian01.lim@gmail.com>
2018-06-07 16:02:31 -0700

    finally

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -256,7 +256,7 @@ public class MainServlet extends HttpServlet {
                     System.err.println(query);
                 }
                 System.err.println(query);
-                String json = new Gson().toJson(new ArrayResponse(query));
+                String json = new Gson().toJson(query);
                 resp.getWriter().write(json);
             } else if (method.equals("user")) {
                 String userID = req.getHeader("email");
